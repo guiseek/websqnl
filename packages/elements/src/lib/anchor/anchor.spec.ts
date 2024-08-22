9433ea996bf3177a3b6a5de8cda3d8a2c3c02e71
Gui Seek <guiseek@gmail.com>
2024-08-22 09:55:57 -0300

    test(elements): fix anchor test

--- packages/elements/src/lib/anchor/anchor.spec.ts
+++ packages/elements/src/lib/anchor/anchor.spec.ts
@@ -2,9 +2,9 @@ import {Anchor} from './anchor'
 
 describe('anchor', () => {
   it('should create instance', () => {
-    const element = new Anchor('Text', '#')
+    const element = new Anchor({href: '#'})
 
     expect(element).toBeInstanceOf(Anchor)
-    expect(element.href).toBe('#')
+    expect(element.href).toBe('http://localhost/#')
   })
 })
