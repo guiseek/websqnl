ab0dcfa11001092cf6d0128055f51350114530ca
Gui Seek <guiseek@gmail.com>
2024-08-23 06:07:03 -0300

    feat(event-flow): export types

--- packages/event-flow/src/index.ts
+++ packages/event-flow/src/index.ts
@@ -1,4 +1,6 @@
 export * from './lib/create-listener'
 export * from './lib/create-target'
 export * from './lib/create-event'
+export * from './lib/state-event'
 export * from './lib/dispatch'
+export * from './lib/types'
